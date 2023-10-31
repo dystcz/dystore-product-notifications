9bee81a301414b673e767835f2cf10db452cd53e
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-31 09:50:43 +0100

    add validation messages

---
+++ lang/en/validations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'store_product_notification' => [
+        'email' => [
+            'required' => 'Please enter your email',
+            'email' => 'Please enter a valid email',
+            'unique' => 'You are already subscribed to this product',
+        ],
+    ],
+];
