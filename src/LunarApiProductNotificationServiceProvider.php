b46535671341cb9d4e29c3d7f042dca2375e5176
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-31 08:13:18 +0100

    update sp

--- src/LunarApiProductNotificationServiceProvider.php
+++ src/LunarApiProductNotificationServiceProvider.php
@@ -2,26 +2,25 @@
 
 namespace Dystcz\LunarApiProductNotification;
 
+use Dystcz\LunarApi\Base\Facades\SchemaManifestFacade;
+use Dystcz\LunarApi\Support\Config\Collections\DomainConfigCollection;
+use Dystcz\LunarApiProductNotification\Domain\ProductNotifications\JsonApi\V1\ProductNotificationSchema;
 use Dystcz\LunarApiProductNotification\Domain\ProductNotifications\Models\ProductNotification;
 use Dystcz\LunarApiProductNotification\Domain\ProductNotifications\Observers\ProductVariantObserver;
-use Dystcz\LunarApiProductNotification\Domain\ProductNotifications\Policies\ProductNotificationPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Lunar\Models\ProductVariant;
 
 class LunarApiProductNotificationServiceProvider extends ServiceProvider
 {
-    protected $policies = [
-        ProductNotification::class => ProductNotificationPolicy::class,
-    ];
-
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(): void
     {
+        $this->registerSchemas();
+        $this->registerObservers();
         $this->registerDynamicRelations();
-
-        ProductVariant::observe(ProductVariantObserver::class);
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
@@ -36,16 +35,48 @@ class LunarApiProductNotificationServiceProvider extends ServiceProvider
     /**
      * Register the application services.
      */
-    public function register()
+    public function register(): void
     {
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/lunar-api-product-notifications.php', 'lunar-api-product-notifications');
     }
 
+    /**
+     * Register schemas.
+     */
+    public function registerObservers(): void
+    {
+        ProductVariant::observe(ProductVariantObserver::class);
+    }
+
+    /**
+     * Register schemas.
+     */
+    public function registerSchemas(): void
+    {
+        SchemaManifestFacade::registerSchema(ProductNotificationSchema::class);
+    }
+
+    /**
+     * Register dynamic relations.
+     */
     protected function registerDynamicRelations(): void
     {
         ProductVariant::resolveRelationUsing('notifications', function ($model) {
             return $model->morphMany(ProductNotification::class, 'purchasable');
         });
+    }
+
+    /**
+     * Register the application's policies.
+     */
+    public function registerPolicies(): void
+    {
+        DomainConfigCollection::fromConfig('lunar-api-product-notifications.domains')
+            ->getPolicies()
+            ->each(
+                fn (string $policy, string $model) => Gate::policy($model, $policy),
+            );
+
     }
 }
